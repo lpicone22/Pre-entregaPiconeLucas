020d9e83480af900ca9cef2686ea960272ac76d5
proyectojs <lucaspicone22@gmail.com>
2022-11-29 01:24:07 -0300

    agregamosFunciones

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 let mensajeBienvenida = "Encontra el Plan de Salud ideal para vos!";
 alert(mensajeBienvenida);
 
@@ -10,7 +11,35 @@ if(edad < 18) {
     alert("Debes ingresar a la cobertura con un mayor de edad");
 }else if (edad >= 18 && edad <= 59) {
     alert("Contanos más sobre quienes tendrian el servicio!");
-}else if(edad > 60 ) {
-    alert("Lo sentimos por el momento no contamos con opciones disponibles!");
+}else while (edad >= 60  ) { 
+
+alert("Lo sentimos por el momento no contamos con opciones disponibles!")  
+    ;
 }
 console.log (edad)
+
+let sumaConyugue = parseInt(prompt("queres agregar a tu pareja? indicanos su edad Aquí"))
+while (sumaConyugue>=60){
+    alert("lo sentimos no tenemos opciones disponibles")
+}
+let sumaHijos= parseInt(prompt("Tenes Hijos a tu cargo? cuantos?"))
+
+const precioHijogenesis= 1000
+let  precioTitular= (edad<=25)==2000
+let  precioTitular2grupo= (edad<=26)==3000
+
+
+function pagaxHijos(){
+    return precioHijogenesis * sumaHijos
+}
+
+let valorPagar=pagaxHijos();
+console.log (valorPagar)
+
+alert(valorPagar)
+
+
+
+
+
+
